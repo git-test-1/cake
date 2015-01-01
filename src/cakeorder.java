27697e4a28d3e00de15a28997e11448e5cbad2be
Colin <colinreadman@tiscali.co.uk>
2015-01-01 11:19:12 +0000

    cake 1st change

---
+++ src/cakeorder.java
@@ -0,0 +1,24 @@
+
+public class cakeorder {
+	
+	private int quantity;
+	
+	/**
+	 * 
+	 * @param args
+	 */
+	public void setQuantity(Integer i) {
+	
+		quantity=i;
+		
+	}
+	
+	/**
+	 * @param args
+	 */
+	public Integer getQuantity() {
+	
+		return quantity;
+		
+	}	
+}
